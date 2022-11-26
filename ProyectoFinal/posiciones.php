69ea384f683e3baf5ec4443d1aac8f4779187a29
David <david@mail.info7.com>
2022-11-26 17:15:17 -0500

    hola

--- ProyectoFinal/posiciones.php
+++ ProyectoFinal/posiciones.php
@@ -1,3 +1,8 @@
+<?php
+    $conexion=mysqli_connect('localhost','root','','futbol');
+?>
+
+
 <!DOCTYPE html>
 <head>
     <title> Tabla de Posiciones</title>
@@ -12,8 +17,44 @@
 
 <body>
 
-<div>
-   TABLA DE POSICIONES YADA YADA.
-</div>
+    <table class="tabla_p">
+        <tr class="ffff">
+            <td>Equipo</td>
+            <td>Grupo</td>
+            <td>PJ</td>
+            <td>G</td>
+            <td>E</td>
+            <td>P</td>
+            <td>GF</td>
+            <td>DG</td>
+            <td>Puntos</td>
+        </tr>
+
+        <?php
+            $sql="SELECT * from teams";
+            $result=mysqli_query($conexion,$sql);
+
+            while($mostrar=mysqli_fetch_array($result)){
+        ?>
+
+         <tr>
+            <td> <?php echo $mostrar['country']?> </td>
+            <td> <?php echo $mostrar['group']?> </td>
+            <td> <?php echo $mostrar['pj']?> </td>
+            <td> <?php echo $mostrar['win']?> </td>
+            <td> <?php echo $mostrar['draw']?> </td>
+            <td> <?php echo $mostrar['loss']?> </td>
+            <td> <?php echo $mostrar['goals_favor']?> </td>
+            <td> <?php echo $mostrar['goals_against']?> </td>
+            <td> <?php echo $mostrar['dg']?></td>
+            <td> <?php echo $mostrar['pts']?> </td>
+        </tr>
+        <?php
+            }
+        ?>
+        
+        
+
+    </table>
 
 </body>
